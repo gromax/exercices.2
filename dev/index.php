ea8a9ab3215d21c776600ead41821d9aeda3257f
gromax <gomaxx@gmail.com>
2019-06-04 22:22:01 +0200

    suppression de la partie debug dans le index.php

--- dev/index.php
+++ dev/index.php
@@ -1,19 +1,5 @@
 <?php
 include "../php/constantes.php";
-
-if (DEBUG)
-{
-  // Pour le dev
-  // Afficher les erreurs à l'écran
-  ini_set('display_errors', 1);
-  // Enregistrer les erreurs dans un fichier de log
-  ini_set('log_errors', 1);
-  // Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
-  ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
-  // Afficher les erreurs et les avertissements
-  error_reporting(E_ALL);
-}
-
 ?>
 <!DOCTYPE html>
 <html>
